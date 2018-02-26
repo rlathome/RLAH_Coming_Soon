91ab3e3960ca7eb05dd20264438168e05d356208
Josh Foster <josh@allenb.com>
2018-02-26 14:49:12 +0700

    misc

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('info', index);
+app.use('/info', index);
 
 app.use('/',express.static('client/build'));
 

13a8050404e05fc1715572976be2c96a15a290db
Josh Foster <josh@allenb.com>
2018-02-26 14:40:02 +0700

    misc

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 var mongoose = require('mongoose');
@@ -18,24 +18,25 @@ app.set('view engine', 'ejs');
 console.log('port: ',process.env.PORT);
 
 mongoose.connect(
-  process.env.DB_CONN_URL,{
+  process.env.DB_CONN_TEST,{
+      useMongoClient:true
+  });
 
-});
+
 app.use(cors());
-
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/users', users);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./client/build'));
+}
+// app.use('/',express.static('client/build'));
 app.use('/info', index);
-
 app.use('/',express.static('client/build'));
-
 
 app.get('*', function (request, response){
   response.sendFile(path.resolve(__dirname, 'build', 'index.html'))
@@ -55,11 +56,14 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status('hey - '+err.status || 500);
+  res.status(err.status || 500);
   res.render('error');
 });
 
-const PORT = process.env.PORT || 8088;
+
+
+const PORT = process.env.PORT || 8080;
+
 
 
 app.listen(PORT, () => {

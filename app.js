b46cf1162d6456970d6fe75a7393fad06c7a7f49
Josh Foster <josh@allenb.com>
2018-02-26 14:34:30 +0700

    misc

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/info', index);
+
 app.use('/',express.static('client/build'));
 
 
@@ -54,7 +55,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status('hey - '+err.status || 500);
   res.render('error');
 });
 
